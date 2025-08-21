273da5084b89199a7fbc5fabad3f435613b41f6b
seohyun27 <canna7610@gmail.com>
2025-08-21 21:49:44 +0900

    feat: 메인 코드 업로드

---
+++ breadcast/src/main/java/com/breadcrumbs/breadcast/BreadCastApplication.java
@@ -0,0 +1,13 @@
+package com.breadcrumbs.breadcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadCastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreadCastApplication.class, args);
+	}
+
+}
---
+++ breadcast/src/test/java/com/breadcrumbs/breadcast/BreadCastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breadcrumbs.breadcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadCastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
