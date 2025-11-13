6ca3ed6527e390cee6bd06b8a994933a65a347d3
Uzrt <wakhj47@naver.com>
2025-11-13 21:05:55 +0900

    가게 정보 보기 기능 초안 수정

--- breadcast/src/main/java/com/breadcrumbs/breadcast/controller/BakeryController.java
+++ breadcast/src/main/java/com/breadcrumbs/breadcast/controller/BakeryController.java
@@ -7,6 +7,7 @@ import com.breadcrumbs.breadcast.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class BakeryController {
     private final BakeryService bakeryService;
     private final ReviewService reviewService;
 
+    @GetMapping("/api/bakeries/{bakeryId}")
     public BakeryDetailResponse getBakeryDetail(@PathVariable Long bakeryId,
                                                 @AuthenticationPrincipal UserDetailsImpl userDetails){
         return bakeryService.getBakeryDetail(bakeryId, userDetails.getUserId());
