b67c4a1265b108ae47c6a51d0375116618f30ddb
Uzrt <wakhj47@naver.com>
2025-11-14 20:01:18 +0900

    BakeryReportRepository, CourseRepository 오류 수정

--- breadcast/src/main/java/com/breadcrumbs/breadcast/repository/bakery/BakeryReportRepository.java
+++ breadcast/src/main/java/com/breadcrumbs/breadcast/repository/bakery/BakeryReportRepository.java
@@ -13,7 +13,7 @@ public interface BakeryReportRepository extends JpaRepository<BakeryReport, Long
     // Member 기준으로 빵집 제보 리스트 조회
     List<BakeryReport> findByMemberId(Long memId);
 
-    List<BakeryReport> findByBakeryIdOrderByCreatedAtDesc(Long bakeryId);
+    List<BakeryReport> findByBakeryIdOrderByDateDesc(Long bakeryId);
     /*
     bakeryId와 일치하는 BakeryReport 엔티티들을 최신순으로 정렬하여 반환합니다.
     Pageable 객체를 파라미터로 받습니다.
@@ -21,6 +21,6 @@ public interface BakeryReportRepository extends JpaRepository<BakeryReport, Long
     */
 
     // createdAt으로 주어진 시각 이전에 생성된 모든 제보 글을 찾아 리스트로 반환
-    List<BakeryReport> findByCreatedAtBefore(LocalDateTime createdAt);
+    List<BakeryReport> findByDateBefore(LocalDateTime date);
 
 }
--- breadcast/src/main/java/com/breadcrumbs/breadcast/repository/course/CourseRepository.java
+++ breadcast/src/main/java/com/breadcrumbs/breadcast/repository/course/CourseRepository.java
@@ -15,6 +15,6 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
 
     // 제목 또는 키워드에 특정 문자열이 포함된 코스 목록을 검색합니다.
     // Pageable 객체를 사용하여 페이징 및 정렬을 함께 처리합니다.
-    List<Course> findByTitleContainingOrKeywordContaining(String title, String keyword);
+    List<Course> findByTitleContaining(String title);
 
 }
