5f1813c628b81c7854eb24dccb3edabf278763d5
Uzrt <wakhj47@naver.com>
2025-11-16 23:37:24 +0900

    BakeryRepository에 findByNameContainingIgnoreCase() 추가

--- breadcast/src/main/java/com/breadcrumbs/breadcast/repository/bakery/BakeryRepository.java
+++ breadcast/src/main/java/com/breadcrumbs/breadcast/repository/bakery/BakeryRepository.java
@@ -2,10 +2,12 @@ package com.breadcrumbs.breadcast.repository.bakery;
 
 import com.breadcrumbs.breadcast.domain.bakery.Bakery;
 import com.breadcrumbs.breadcast.domain.bakery.BakeryReport;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BakeryRepository extends JpaRepository<Bakery, Long> {
+    List<Bakery> findByNameContainingIgnoreCase(String keyword);
 }
 
